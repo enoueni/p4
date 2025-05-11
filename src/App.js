f081b53c11e573b1013e8482b2e98f54423496d7
Eric Noueni <nouenieric01@gmail.com>
2025-05-11 18:31:10 -0500

    modified App.js to be Hey Monona, WI!!!!

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello Monona   and Madison, WI!!!!</h1>
+      <h1>Hello Monona and Madison, WI!!!!</h1>
     </div>
   );
 }
